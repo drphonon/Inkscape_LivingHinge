cca5ba7c07f7ec8729fb97ccce3c69bc4f5fcf57
Mark Endicott <drphonon@gmail.com>
2016-11-27 20:17:55 -0500

    Update copy_files.sh
    Changed location of the extension files per github user Moini

--- copy_files.sh
+++ copy_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-export SUDO_ASKPASS=/usr/bin/ssh-askpass 
-sudo cp living_hinge.inx /usr/share/inkscape/extensions/
-sudo cp src/living_hinge.py /usr/share/inkscape/extensions/
+#export SUDO_ASKPASS=/usr/bin/ssh-askpass 
+#sudo cp living_hinge.inx /usr/share/inkscape/extensions/
+#sudo cp src/living_hinge.py /usr/share/inkscape/extensions/
+cp living_hinge.inx ~/.config/inkscape/extensions
+cp src/living_hinge.py ~/.config/inkscape/extensions
